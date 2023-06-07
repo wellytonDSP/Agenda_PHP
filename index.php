cf63ef9a9a86955109bdd7f7d5339a905c4009ed
WellytonDSP <wellyton.santos@outlook.com>
2023-06-07 17:42:11 -0300

    Primeiro_arquivos

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+    include_once("templates/header.php");
+
+?>
+
+
+<?php
+
+    include_once("templates/footer.php");
+?>
---
+++ templates/header.php
@@ -0,0 +1,23 @@
+<?php
+
+include_once("config/url.php");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Boostrap 5 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" integrity="sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Css3 -->
+    <link rel="stylesheet" href="<?=$BASE_URL?>css/style.css">
+    <title>Agenda</title>
+</head>
+<body>
+    
